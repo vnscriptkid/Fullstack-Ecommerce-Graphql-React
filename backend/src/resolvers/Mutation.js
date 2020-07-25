0a6fc8345812aee32f7dfb75de0e7338adc71509
Thanh Nguyen <vnscriptkid@gmail.com>
2020-07-25 22:11:35 +0700

    [Backend] Query items and create new item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutation = {};
+const Mutation = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: check if user are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+};
 
 module.exports = Mutation;
