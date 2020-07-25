f592ca664a95dec4ccb16c981bcc034961e2efaf
Thanh Nguyen <vnscriptkid@gmail.com>
2020-07-25 23:31:23 +0700

    [Client] Hook up ApolloClient to React

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,35 @@
 import App, {Container} from 'next/app';
 import Page from '../components/Page';
 import Meta from '../components/Meta';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        const pageProps = {}
+
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        pageProps.query = ctx.query;   
+        return { pageProps }
+    }
+    
     render() {
-        const {Component} = this.props;
+        const {Component, apollo, pageProps} = this.props;
         return (
             <Container>
                 {/* shared states */}
-                <Meta />
-                <Page>
-                    <Component />
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Meta />
+                    <Page {...pageProps}>
+                        <Component />
+                    </Page>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-export default MyApp;
+export default withData(MyApp);
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,6 @@
-import Link from 'next/link';
-
 const Home = (props) => (
     <div>
         <div>You are home</div>
-        <Link href="/sale">
-            <a>Sale</a>
-        </Link>
     </div>
 )
 
