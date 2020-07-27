c0cdf3309dfbb1a8c77e7b0c59e5778686aabc14
Thanh Nguyen <vnscriptkid@gmail.com>
2020-07-27 13:17:35 +0700

    [Frontend] Items pagination

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -3,10 +3,12 @@ import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
+import Pagination from './Pagination';
+import {perPage} from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY($skip:Int, $first:Int) {
+        items(skip: $skip, first: $first) {
             id
             title
             description
@@ -30,16 +32,28 @@ const ItemList = styled.div`
 `;
 
 class Items extends Component {
+    
+    renderPagination = () => (
+        <Pagination current={this.props.page}/>
+    );
+    
     render() {
+        const skip = perPage * this.props.page - perPage;
         return (
             <Center>
-                <Query query={ALL_ITEMS_QUERY}>
-                    {({ data, error, loading }) => {
-                        if (error) return <p>Error</p>
-                        if (loading) return <p>Loading...</p>
-                    return <ItemList>{data.items.map(item => <Item key={item.id} item={item}/>)}</ItemList>
-                    }}
-                </Query>
+                    {this.renderPagination()}
+                        <Query query={ALL_ITEMS_QUERY} variables={{ skip, first: perPage }}>
+                            {({ data, error, loading }) => {
+                                if (error) return <p>Error</p>
+                                if (loading) return <p>Loading...</p>
+                                return (
+                                    <ItemList>
+                                            {data.items.map(item => <Item key={item.id} item={item}/>)}
+                                    </ItemList>
+                                );
+                            }}
+                        </Query>    
+                    {this.renderPagination()}
             </Center>
         );
     }
---
+++ frontend/components/Pagination.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
+import {perPage} from '../config';
+import Head from 'next/head';
+import Link from 'next/link';
+
+const ALL_ITEMS_COUNT = gql`
+    query ALL_ITEMS_COUNT {
+        itemsConnection {
+            aggregate{
+                count
+            }
+        }
+    }
+`;
+
+const Pagination = ({ current }) => {
+    return (
+        <Query query={ALL_ITEMS_COUNT}>
+            {({ data, error, loading }) => {
+                if (error) return <p>Error</p>
+                if (loading) return <p>Loading...</p>
+                const count = data.itemsConnection.aggregate.count;
+                const total = Math.ceil(count / perPage);
+                return (
+                    <PaginationStyles>
+                        <Head>
+                            <title>Sick Fits - Page {current} of {total}</title>
+                        </Head>
+                        <Link className="prev" prefetch href={{ pathname: '/items', query: { page: current - 1 } }}>
+                            <a aria-disabled={current <= 1}>Prev</a>
+                        </Link>
+                        <p>Page {current} of {total}</p>
+                        <Link className="next" prefetch href={{ pathname: '/items', query: { page: current + 1 } }}>
+                            <a aria-disabled={current >= total}>Next</a>
+                        </Link>
+                    </PaginationStyles>
+                );
+            }}
+        </Query>
+    );
+};
+
+export default Pagination;
