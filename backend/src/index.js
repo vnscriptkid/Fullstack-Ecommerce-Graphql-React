93d22ded9bcd7175e52311a948506111c0a98358
Thanh Nguyen <vnscriptkid@gmail.com>
2020-07-27 22:44:18 +0700

    [Backend] Get current user by token

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({
     path: 'variables.env'
 });
@@ -9,6 +10,17 @@ const server = createServer();
 
 // middleware
 server.express.use(cookieParser());
+
+server.express.use(function(req, res, next) {
+    const token = req.cookies.token;
+
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        req.userId = userId;
+    }
+    
+    next();
+});
 
 server.start(
     {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,22 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
+    itemsConnection: forwardTo('db'),
+
+    async me(parent, args, ctx, info) {
+        const userId = ctx.request.userId;
+
+        if (!userId) {
+            return null;
+        }
+        // user(where: UserWhereUniqueInput!): User
+        return ctx.db.query.user(
+            { 
+                where: { id: userId } 
+            }, 
+            info
+        );
+    }   
     
     // async items(parent, args, ctx, info) {
     //     const items = await ctx.db.query.items()
