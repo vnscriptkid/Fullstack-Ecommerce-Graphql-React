3d997e0ff53ee279e24d5b17318e4783d3f4a9e1
Thanh Nguyen <vnscriptkid@gmail.com>
2020-08-05 23:10:23 +0700

    [Frontend] Improve UX when deleting cartItem by optimistic response

--- frontend/components/RemoveCartItem.js
+++ frontend/components/RemoveCartItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import {Mutation} from 'react-apollo';
 import gql from 'graphql-tag';
@@ -27,22 +27,47 @@ const BigButton = styled.button`
     }
 `;
 
-const RemoveCartItem = ({ cartItemId }) => {
-    return (
-        <Mutation mutation={REMOVE_CART_ITEM} variables={{ cartItemId }} refetchQueries={[{ query: GET_CURRENT_USER }]}>
-            {(removeFromCart, { loading, error }) => {
-                return (
-                    <BigButton disabled={loading} onClick={() => {
-                        removeFromCart().catch(err => alert(err.message));
-                    }}>&times;</BigButton>
-                )
-            }}
-        </Mutation>
-    );
-};
+class RemoveCartItem extends Component {
 
-RemoveCartItem.propTypes = {
-    cartItemId: PropTypes.string.isRequired
+    static propTypes = {
+        cartItemId: PropTypes.string.isRequired
+    }
+
+    afterSuccessfulDelete = (cache, payload) => {
+        // read data from cache
+        const _data = cache.readQuery({ query: GET_CURRENT_USER });
+        const data = JSON.parse(JSON.stringify(_data));
+        // fitler the deleted one from data
+        const deletedCartItemId = payload.data.removeFromCart.id;
+        data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== deletedCartItemId);
+        // write new data to cache
+        cache.writeQuery({ query: GET_CURRENT_USER, data })
+    }
+    
+    render() {
+        return (
+            <Mutation 
+                mutation={REMOVE_CART_ITEM} 
+                variables={{ cartItemId: this.props.cartItemId }} 
+                update={this.afterSuccessfulDelete}
+                optimisticResponse={{
+                    __typename: 'Mutation',
+                    removeFromCart: {
+                        __typename: 'CartItem',
+                        id: this.props.cartItemId
+                    }
+                }}
+            >
+                {(removeFromCart, { loading, error }) => {
+                    return (
+                        <BigButton disabled={loading} onClick={() => {
+                            removeFromCart().catch(err => alert(err.message));
+                        }}>&times;</BigButton>
+                    )
+                }}
+            </Mutation>
+        );
+    }
 }
 
 export default RemoveCartItem;
